9460c5e224b896a069c9ee928b718b3b9b539850
Miles Kenner <mdk0109@yahoo.com>
2019-02-13 19:06:41 -0600

    api keys added

---
+++ liri.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+var concertThis = function(name){
+
+}
+
+var spotifyThisSong = function(song){
+
+}
+
+var movieThis = function(movie){
+
+}
+
+var doWhatItSays = function(listen){
+    
+}
